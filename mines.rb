ea98e4101a624c6f3a321eb0a61ebf8d5c2490a0
Otto Henrique <ottohenrique@gmail.com>
2017-09-07 16:16:31 -0300

    divide gameloop da engine/ tamanho configuravel do tabuleiro

--- mines.rb
+++ mines.rb
@@ -1,108 +1,31 @@
 require './pretty_printer'
 require './basic_printer'
+require './game'
 
-class Mines
-  attr_accessor :rows, :cols
-
-  def initialize(rows, cols, mines)
-    @board = Array.new(rows) { Array.new(cols) }
-    @state = Array.new(rows) { Array.new(cols) { '.' }}
-
-    @rows = rows
-    @cols = cols
-
-    fill_bombs(mines)
-    fill_numbers
-  end
-
-  def board_state
-    @state
-  end
-
-  def play(x, y)
-    return false if @state[x][y] != '.'
-
-    if @board[x][y] != 'x'
-      @state[x][y] = @board[x][y]
-
-      if @board[x][y] == ' '
-        neighbors(x, y).each do |neigh|
-          play(neigh[0], neigh[1])
-        end
-      end
-    else
-      @state[x][y] = 'X'
-      raise 'Booooom!'
-    end
-
-    true
-  end
-
-  def flag(x, y)
-    case @state[x][y]
-    when '.'
-      @state[x][y] = '?'
-    when '?'
-      @state[x][y] = '.'
-    else
-      false
-    end
-  end
-
-  def fill_bombs(mines)
-    while mines > 0
-      x = rand(@rows)
-      y = rand(@cols)
-
-      if @board[x][y].nil?
-        @board[x][y] = 'x'
-        mines -= 1
-      end
-    end
-  end
-
-  def fill_numbers
-    @board.each_with_index do |row, x|
-      row.each_with_index do |col, y|
-        if @board[x][y] != 'x'
-          bombs = 0
-          neighbors(x, y).each do |neigh|
-            bombs += 1 if @board[neigh[0]][neigh[1]] == 'x'
-          end
-          @board[x][y] = bombs > 0 ? bombs : ' '
-        end
-      end
-    end
-  end
-
-  def neighbors(x,y)
-    n = []
-
-    [-1, 0, 1].each do |i|
-      [-1, 0, 1].each do |j|
-        unless (i == 0 && j == 0) ||
-            (x + i < 0 || x + i >= @rows) ||
-            (y + j < 0 || y + j >= @cols)
-          n << [x + i, y+j]
-        end
-      end
-    end
-
-    n
-  end
-end
-
-
-mines = Mines.new(5, 8, 5)
 
 print "Deseja jogar com cores? (Y/n) > "
 colors = STDIN.gets.chomp.downcase
+
+print "Linhas (5) > "
+rows = STDIN.gets.chomp
+rows = rows.empty? ? 5 : rows.to_i
+
+print "Colunas (8) > "
+cols = STDIN.gets.chomp
+cols = cols.empty? ? 8 : cols.to_i
+
+print "Bombas (15) > "
+bombs = STDIN.gets.chomp
+bombs = bombs.empty? ? 8 : bombs.to_i
+
 
 if colors == 'n'
   @printer = BasicPrinter
 else
   @printer = PrettyPrinter
 end
+
+mines = Game.new(rows, cols, bombs)
 
 begin
   while true
